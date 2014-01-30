3b393a1d16677a24dc1608b924ad6a610a529ccb
James Lal <james@lightsofapollo.com>
2014-01-30 04:03:06 -0800

    ngrok config + dependencies (no tests/docs)

---
+++ ngrok_services.js
@@ -0,0 +1,15 @@
+if (!process.env.NGROK_AUTH) {
+  console.error('NGROK_AUTH environment variable is missing');
+  console.error('  go to https://ngrok.com/ to get a key');
+  process.exit(1);
+}
+
+console.log('XX');
+
+module.exports = {
+  queue: { port: 8314 },
+  provisioner: { port: 3000 },
+  rabbitmq: {
+    port: 5672, proto: 'tcp', authtoken: process.env.NGROK_AUTH
+  }
+};
