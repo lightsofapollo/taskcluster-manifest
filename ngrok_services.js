efef8582bc4c5cb01496dca1f01d8d334134538d
James Lal <james@lightsofapollo.com>
2014-01-31 11:20:09 -0800

    update ngrok services

--- ngrok_services.js
+++ ngrok_services.js
@@ -1,13 +1,9 @@
-if (!process.env.NGROK_AUTH) {
-  console.error('NGROK_AUTH environment variable is missing');
-  console.error('  go to https://ngrok.com/ to get a key');
-  process.exit(1);
-}
-
 module.exports = {
-  queue: { port: 8314 },
-  provisioner: { port: 3000 },
-  rabbitmq: {
-    port: 5672, proto: 'tcp', authtoken: process.env.NGROK_AUTH
+  queue_host: { port: 8314 },
+  provisioner_host: { port: 3000 },
+  rabbitmq_host: {
+    port: 5672,
+    proto: 'tcp',
+    authtoken: 'qBGjbalYXUx307omJI8r'
   }
 };
